ab0b657a2eb0e7f7cafa47edfecbd70162e23212
无涯 <468690618@qq.com>
2023-02-20 15:45:49 +0800

    use unplugin

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,6 +2,10 @@
 
 <template>
 	<div>this is home</div>
+	<n-space>
+		<n-button type="info" dashed> Info </n-button>
+		<n-button type="primary" dashed> Primary </n-button>
+	</n-space>
 </template>
 
 <style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
+
 // https://vitejs.dev/config/
 
 export default defineConfig({
-	plugins: [vue()],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
@@ -19,4 +22,35 @@ export default defineConfig({
 		host: '0.0.0.0',
 		port: 9100,
 	},
+	plugins: [
+		vue(),
+		AutoImport({
+			// 需要去解析的文件
+			include: [
+				/\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+				/\.vue$/,
+				/\.vue\?vue/, // .vue
+				/\.md$/, // .md
+			],
+			// imports 指定自动引入的包位置（名）
+			imports: ['vue', 'pinia', 'vue-router'],
+			// 生成相应的自动导入json文件。
+			eslintrc: {
+				// 启用
+				enabled: true,
+				// 生成自动导入json文件位置
+				filepath: './.eslintrc-auto-import.json',
+				// 全局属性值
+				globalsPropValue: true,
+			},
+			resolvers: [NaiveUiResolver()],
+		}),
+		Components({
+			// imports 指定组件所在目录，默认为 src/components
+			dirs: ['src/components/', 'src/view/'],
+			// 需要去解析的文件
+			include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+			resolvers: [NaiveUiResolver()],
+		}),
+	],
 })
